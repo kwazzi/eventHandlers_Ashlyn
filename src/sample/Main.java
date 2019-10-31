9cf5c2f7c83c5acadf4bfa2e1cdfe02d5bc404c5
stu41471 <ashlyn.kwasnica71@stu.bryanisd.org>
2019-10-31 09:51:51 -0500

    drew a circle

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Pane pane = new Pane();
+        Scene scene = new Scene(pane,500,500);
+
+        Circle circle = new Circle();
+        circle.setRadius(80);
+        circle.setCenterX(250);
+        circle.setCenterY(250);
+        Color circleColor = Color.rgb(205,12,189);
+        circle.setFill(circleColor);
+
+        pane.getChildren().add(circle);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Handle Events");
         primaryStage.show();
     }
 
