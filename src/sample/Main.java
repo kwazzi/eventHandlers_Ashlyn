f8a8dca16de71270412698a82a08f703f0d0572b
kwazzi <54319675+kwazzi@users.noreply.github.com>
2019-11-03 13:10:03 -0600

    Made it a game
    added controls for arrows and made a fruit appear and when u hit it, it appears in a different location

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,14 +1,14 @@
 /*
-//// INSTRUCTIONS ////////////////////////////////
-//  Double Click - changes color to coral       //
-//  Normal Click - changes color to blue        //
-//  Right Click - adds a circle to the corner   //
-//  Shift - makes circle bigger                 //
-//  Enter - makes circle smaller                //
-//  Space - moves the circle                    //
-//////////////////////////////////////////////////
+//// INSTRUCTIONS /////////////////////////////////////
+//  Double Click - changes color to coral            //
+//  Normal Click - changes color to blue             //
+//  Right Click - adds a circle to the corner        //
+//  Arrow Keys - moves the circle                    //
+//  Enter - changes the circles size                 //
+//  Space - moves the circle to a random position    //
+///////////////////////////////////////////////////////
 */
-package sample;
+package eventhandler;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -18,9 +18,11 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -29,6 +31,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Pane pane = new Pane();
         Scene scene = new Scene(pane,500,500);
+        Rectangle rect = drawFruit();
+        pane.getChildren().add(rect);
         
         // creating the main circle
         Circle circle = drawCircle(Color.RED, 80, 250,250);
@@ -37,7 +41,7 @@ public class Main extends Application {
         // creates a new circle when you right click on the main circle
         circle.setOnMouseClicked((MouseEvent event) -> {
             if(event.getButton() == MouseButton.SECONDARY){
-                pane.getChildren().add(drawCircle(Color.BLUE, 20, 50, 50));
+                pane.getChildren().add(drawCircle(Color.DARKSLATEBLUE, 20, 50,50));
             }
             // changes the color of the circle when you double click on the main circle
             else if(event.getClickCount() == 2){
@@ -48,20 +52,51 @@ public class Main extends Application {
                 circle.setFill(Color.CADETBLUE);
             }
         });
+        
 ////////////// KEYEVENTS ///////////////////
+       double speed = 10;
         scene.setOnKeyPressed((KeyEvent event) -> {
             // makes circle smaller if they click enter
             if(event.getCode() == KeyCode.ENTER){
-                circle.setRadius(20);
+                circle.setRadius(Math.random() * 100);
+                moveFruit(rect, circle);
             }
-            // makes circle bigger if they click shift
-            else if(event.getCode() == KeyCode.SHIFT){
-                circle.setRadius(120);
+            // moves the circle whereever they chose
+            else if(event.getCode() == KeyCode.UP){
+                double y = circle.getCenterY() - speed;
+                if(circle.getCenterY() < 0){
+                    y = circle.getCenterY();
+                }
+                circle.setCenterY(y);
+                moveFruit(rect, circle);
+            }
+            else if(event.getCode() == KeyCode.DOWN){
+                double y = circle.getCenterY() + speed;
+                if(circle.getCenterY() > 500){
+                    y = circle.getCenterY();
+                }
+                circle.setCenterY(y);
+                moveFruit(rect, circle);
+            }
+            else if(event.getCode() == KeyCode.RIGHT){
+                double x = circle.getCenterX() + speed;
+                if(circle.getCenterX() > 500){
+                    x = circle.getCenterX();
+                }
+                circle.setCenterX(x);
+            }
+            else if(event.getCode() == KeyCode.LEFT){
+                double x = circle.getCenterX() - speed;
+                if(circle.getCenterX() < 0){
+                    x = circle.getCenterX();
+                }
+                circle.setCenterX(x);
+                moveFruit(rect, circle);
             }
             // moves position of the circle
             else if(event.getCode() == KeyCode.SPACE){
-                circle.setCenterX(220);
-                circle.setCenterY(220);
+                circle.setCenterX(Math.random() * 500);
+                circle.setCenterY(Math.random() * 500);
             }
         });
         
@@ -79,9 +114,32 @@ public class Main extends Application {
         temp.setFill(color);
         return temp;
     }
+    
+    public Rectangle drawFruit(){
+        Rectangle rect = new Rectangle();
+        rect.setHeight(20);
+        rect.setWidth(20);
+        double x = Math.random() * 480;
+        double y = Math.random() * 480;
+        rect.setX(x);
+        rect.setY(y);
+        rect.setFill(Color.DEEPSKYBLUE);
+        return rect;
+    }
+    
+    public void moveFruit(Rectangle rect, Circle circle){
+        boolean a = circle.getCenterX() > rect.getX() && circle.getCenterX() < rect.getX() + rect.getHeight();
+        boolean b = circle.getCenterY() > rect.getY() && circle.getCenterY() < rect.getY() + rect.getWidth();
+        if(a && b){
+            double x = Math.random() * 480;
+            double y = Math.random() * 480;
+            rect.setX(x);
+            rect.setY(y);
+        }
+    }
+    
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
